9340de5a49442342417b99e4a701f118eeb5c204
강 혁구 <khk85@wefor.com>
2019-06-18 13:28:36 +0900

    구구단 소스 입력

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
+<!DOCTYPE html>
 <html>
-<head>test</head>
+<head>
+    <title></title>
+</head>
 <body>
-<h1>hello world</h1>
+<?php
+ 
+echo "<h1>Ex. 구구단 출력하기</h1>";
+ 
+for($i=2;$i<=9;$i++){
+    echo $i."단 입니다.<br/>";
+    for($j=1;$j<=9;$j++){
+        echo $i." X ".$j." = ".($i*$j)."<br/>";
+    }
+    echo "<br/>";
+}
+ 
+?>
 </body>
 </html>
